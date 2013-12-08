94704e6e5a5cc2e88b097dd6d22ea38a0797e24c
Aaron Nichols <anichols@trumped.org>
2013-12-07 23:05:46 -0700

    update

--- setup/setup.sh
+++ setup/setup.sh
@@ -2,10 +2,9 @@
 # Quick setup
 
 # Check dependencies
-
 for dependency in curl git ; do 
-  if [! -e $dependency ]; then
-    echo "FAILED DEPENDENCY: $dependency is not installed... exiting"
+  if [! -e ${dependency} ]; then
+    echo "FAILED DEPENDENCY: ${dependency} is not installed... exiting"
     exit
   fi
 done
@@ -15,7 +14,15 @@ echo "****************************"
 echo " Setting up Janus"
 echo "****************************"
 
-cd $HOME
+cd ${HOME}
 curl -Lo- https://bit.ly/janus-bootstrap | bash
 
-# 
+# Setup overrides 
+
+for config in before after ; do
+  if [-f ~/.vimrc.${config} ];then
+    mv ~/.vimrc.${config} ~/.vimrc.${config}_old
+  fi
+
+  ln -s ~/.janus/setup/_${config} ~/.vimrc.${config}
+done
